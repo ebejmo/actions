420e6ee96d8d40df02f03fccec60b3817fde85fc
ebejmo <94139531+ebejmo@users.noreply.github.com>
2024-08-11 20:28:41 +0200

    bajs

---
+++ src/js/joker.test.js
@@ -0,0 +1,12 @@
+import { dadJoke } from './joker.js';
+
+export async function testDadJoke() {
+  const joke = await dadJoke();
+  const testPasses = Boolean(joke);
+  console.log(joke);
+  console.log('Dad Joke Test:', testPasses ? 'PASSED' : 'FAILED');
+  return testPasses;
+}
+
+testDadJoke();
+console.log('Intentional Syntax Error');
